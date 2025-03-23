0052680aa1d05770ee6cd2affeb1793c15695f6f
mohitmaikhuri03 <mkmaikhuri@gmail.com>
2025-03-24 00:45:48 +0530

    Update One.groovy

--- src/snataak/template/One.groovy
+++ src/snataak/template/One.groovy
@@ -4,7 +4,13 @@ import snataak.common.*
 import snataak.golang.*
 
 def call(String branch, String repoUrl, String credentialsId = null, String reportsDir) {
-    Clean()
-    Clone(branch, repoUrl, credentialsId)
-    Unit(reportsDir)
+    CLEAN = new clean()
+    CLONE = new clone()
+    UNIT = new unit()
+
+    
+    CLEAN.call()
+    CLONE.call(branch, repoUrl, credentialsId)
+    UNIT.call(reportsDir)
+    
 }
