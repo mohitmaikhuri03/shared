de7421483acc969d36fb87ac0e234fea319f257a
mohitmaikhuri03 <mkmaikhuri@gmail.com>
2025-03-23 18:44:46 +0530

    Create clone.groovy

---
+++ src/snataak/common/clone.groovy
@@ -0,0 +1,7 @@
+def call(String branch, String url, String credentialsId = null) {
+    if (credentialsId) {
+        git branch: branch, url: url, credentialsId: credentialsId
+    } else {
+        git branch: branch, url: url
+    }
+}
