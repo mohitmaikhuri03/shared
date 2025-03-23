79aec94f30b067ed7ad7eef799df8db26bf5598c
mohitmaikhuri03 <mkmaikhuri@gmail.com>
2025-03-23 18:49:40 +0530

    Create template.groovy

---
+++ src/pipeline/template.groovy
@@ -0,0 +1,18 @@
+package pipeline
+
+import snataak.common.clean
+import snataak.common.clone
+import snataak.golang.unit
+
+def call(String branch, String repoUrl, String credentialsId = null, String reportsDir) {
+    stage('Cleanup') {
+        clean()
+    }
+
+    stage('Clone Repository') {
+        clone(branch, repoUrl, credentialsId)  
+
+    stage('Run Go Unit Tests') {
+        unit(reportsDir) 
+    }
+}
